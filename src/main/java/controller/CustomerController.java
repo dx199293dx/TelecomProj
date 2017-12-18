59b6aa8fa6439e2c2a7add8db8f12bc89341424d
dx199293dx <dx199293dx@me.com>
2017-12-18 18:43:40 -0500

    customer home page
    please check the customer homepage

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomerController {
 
-	//change for test7
-	
-	//test 8
+	@RequestMapping(value = "/customer")
+	public String customer() {
+		System.out.println("customer Login ");
+		return "customerHomepage";
+	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <a href="admin.spring">admin login</a>
+<a href="customer.spring">customer login</a>
 </body>
 </html>
